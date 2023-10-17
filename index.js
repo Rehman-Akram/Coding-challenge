d603927d9415347c6b62bdf3e066cda1d3d8c4e9
RehmanAkram <rehmanakram03@gmail.com>
2023-10-17 17:26:08 +0500

    assessment completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hello world');
+import { INPUT_DATA } from "./constants.js"
+
+const inputData = INPUT_DATA
+
+// preparing main obj
+const mainObj = inputData.reduce((obj, currentValue)=>{
+    obj[currentValue.nodeId] = currentValue
+    currentValue.children = []
+    return obj
+},{})
+
+// adding parent and siblings data
+inputData.forEach(node=>{
+  const parent = mainObj[node.parentId]
+  if (parent) {
+      mainObj[node.parentId].children.push(node)
+      parent.children.splice(node.previousSiblingId ? 
+        parent.children.findIndex((child)=>child.nodeId === node.previousSiblingId)+1 : 
+        0, 
+        node )
+  }
+})
+
+// getting nodes with no parents
+const outputData = inputData.filter(node => !node.parentId);
+
+// Output
+const finalOutput = outputData.map(node => mainObj[node.nodeId]);
+
